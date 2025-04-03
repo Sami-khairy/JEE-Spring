5b3f30a70a72939fcda21db6c6b3282bf58b88a5
Sami-Khairy <samikhairy2003@gmail.com>
2025-04-03 23:11:59 +0000

    init user-roles project

---
+++ user-roles/src/main/java/ma/khairy/userroles/UserRolesApplication.java
@@ -0,0 +1,13 @@
+package ma.khairy.userroles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRolesApplication.class, args);
+	}
+
+}
