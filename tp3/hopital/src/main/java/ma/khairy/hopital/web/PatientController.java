9b49705bc8ec098a6490d81b84cc98909c1cdddb
Sami-Khairy <samikhairy2003@gmail.com>
2025-04-11 11:20:39 +0100

    pagination

--- tp3/hopital/src/main/java/ma/khairy/hopital/web/PatientController.java
+++ tp3/hopital/src/main/java/ma/khairy/hopital/web/PatientController.java
@@ -2,9 +2,12 @@ package ma.khairy.hopital.web;
 
 import ma.khairy.hopital.entities.Patient;
 import ma.khairy.hopital.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,9 +21,13 @@ public class PatientController {
 
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int p,
+                        @RequestParam(name = "size", defaultValue = "4") int s) {
+        Page<Patient> patientPage = patientRepository.findAll(PageRequest.of(p,s));
+        model.addAttribute("patients", patientPage.getContent());
+        model.addAttribute("pages", new int[patientPage.getTotalPages()]);
+        model.addAttribute("currentPage", p);
         return "patients";
     }
 }
