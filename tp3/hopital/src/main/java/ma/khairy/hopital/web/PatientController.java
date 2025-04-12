214702c05a044e4e52ac9c4a9250f8e7c892e3c0
Sami-Khairy <samikhairy2003@gmail.com>
2025-04-12 22:26:09 +0100

    editer patient

--- tp3/hopital/src/main/java/ma/khairy/hopital/web/PatientController.java
+++ tp3/hopital/src/main/java/ma/khairy/hopital/web/PatientController.java
@@ -57,9 +57,21 @@ public class PatientController {
     }
 
     @PostMapping("/save")
-    public String save(Model model, @Valid Patient patient, BindingResult bindingResult){
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,
+                       @RequestParam(name = "keyword", defaultValue = "") String keyword,
+                       @RequestParam(name = "page", defaultValue = "0") int page){
         if (bindingResult.hasErrors()) return "formPatients";
         patientRepository.save(patient);
-        return "redirect:/formPatients";
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/editPatient")
+    public String editPatient(Model model, Long id, String keyword, int page){
+        Patient patient =  patientRepository.findById(id).orElse(null);
+        if(patient == null) throw new RuntimeException("Patient not found");
+        model.addAttribute("patient",patient);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword",keyword);
+        return "editPatient";
     }
 }
